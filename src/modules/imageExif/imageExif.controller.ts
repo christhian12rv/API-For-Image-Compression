4818d38905ae7ea1dd62d92483e4339b51d77f12
Christhian Rezende Vieira <christhian.vieira@morpheustecnologia.com>
2023-05-22 23:09:49 -0300

    refactor: remove unnecessary useFilter of imageExif Controller

--- src/modules/imageExif/imageExif.controller.ts
+++ src/modules/imageExif/imageExif.controller.ts
@@ -1,10 +1,8 @@
-import { Body, Controller, Post, UseFilters } from '@nestjs/common';
-import { HttpExceptionFilter } from '../../filters/httpException.filter';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ImageExifService } from './imageExif.service';
 import { CompressView } from './view/compress.view';
 
 @Controller('imageExif')
-@UseFilters(new HttpExceptionFilter())
 export class ImageExifController {
   constructor(private service: ImageExifService) {}
 
